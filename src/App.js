612b23f01fa615622ec14c1c528afeec7c79202d
hossainalamin <hossainalamin980@gmail.com>
2023-03-04 22:45:02 +0600

    project layouts and component create

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import './App.css';
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import Main from './layouts/Main';
 import Home from './components/Home/Home';
+import Login from './components/Login/Login';
+import Register from './components/Register/Register';
 
 function App() {
   const router = createBrowserRouter([
@@ -13,10 +15,18 @@ function App() {
         {
           path : '/',
           loader : async()=>{
-            return fetch('')
+            return fetch('https://jsonplaceholder.typicode.com/posts')
           },
           element : <Home></Home>
-        }
+        },
+        {
+          path : '/login',
+          element : <Login></Login>
+        },
+        {
+          path : '/register',
+          element : <Register></Register>
+        },
       ]
     }
   ])
